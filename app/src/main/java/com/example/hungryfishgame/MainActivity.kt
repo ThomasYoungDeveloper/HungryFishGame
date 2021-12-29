6b5810022cadb054028c0326d41c89649b2c24b6
Thomas Young <Thomas.young228@gmail.com>
2021-12-28 22:46:27 -0500

    -set up fish and random number gen.

--- app/src/main/java/com/example/hungryfishgame/MainActivity.kt
+++ app/src/main/java/com/example/hungryfishgame/MainActivity.kt
@@ -2,12 +2,19 @@ package com.example.hungryfishgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.os.Handler
+import android.os.Looper
 import android.widget.TextView
+import android.widget.Toast
 
 private var tvBubble : TextView? = null
+var tvFish : TextView? = null
 
+private var fishNumber = 0
 
+val mainHandler = Handler(Looper.getMainLooper())
+
+//String[] numberArray
 
 
 class MainActivity : AppCompatActivity() {
@@ -20,13 +27,42 @@ class MainActivity : AppCompatActivity() {
 
 
         fun numGenerator() {
-            var randomNum = (0..10).random().toString()
-            tvBubble!!.setText(randomNum)
+            var randomNum = (0..10 + 1).random().toString()
+            tvBubble!!.text = randomNum
+
+            randomNum = (0..10 + 1).random().toString()
+            //tvFish!!.text = randomNum
         }
+
+//        fun changeFishNumber() {
+//            var randomNum = (0..15 + 1).random().toString()
+//            tvFish?.text = randomNum
+//        }
 
         tvBubble?.setOnClickListener{
             numGenerator()
         }
+
+        tvFish?.setOnClickListener{
+            numGenerator()
+        }
+
+
+//        for (i in 1..10) {
+//            changeFishNumber()
+//            if (tvFish.text == )
+//            Toast.makeText(this, "It worked. $i", Toast.LENGTH_LONG).show()
+//        }
+
+
+        mainHandler.post(object : Runnable {
+            override fun run() {
+                numGenerator()
+                mainHandler.postDelayed(this, 5000)
+            }
+        })
+
+
 
     }
 }
